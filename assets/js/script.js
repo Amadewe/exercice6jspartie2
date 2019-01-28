7f30dbb45b7f7838c8ad056082f4832543c26c67
Amadewe <dewevre.amandine@gmail.com>
2019-01-28 15:14:28 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,9 @@
-// L'instruction switch évalue une expression et, selon le résultat obtenu et le cas associé, exécute les instructions correspondantes.
-function changeimage(n) {
-switch (n) {
-case 1:
-document.getElementById("image1").src="assets/img/image1_2.jpg";
-break;
-case 2:
-document.getElementById("image2").src="assets/img/image2_2.jpg";
-break;
-case 3:
-document.getElementById("image3").src="assets/img/image3_2.jpg";
-break;
-case 4:
-document.getElementById("image4").src="assets/img/image4_2.jpg";
-break;
-case 5:
-document.getElementById("image5").src="assets/img/image5_2.jpg";
-break;
+// changer l'image au survol de celle-ci par la deuxième pour les 5 images en 1 seule fonction
+// Ici nous allons chercher la deuxième image
+function changeImage(img){
+  document.getElementById(img).src ='assets/images/' + img + '_2.jpg';
 }
-}
-function remettreimage(n) {
-switch (n) {
-case 1:
-document.getElementById("image1").src="assets/img/image1.jpg";
-break;
-case 2:
-document.getElementById("image2").src="assets/img/image2.jpg";
-break;
-case 3:
-document.getElementById("image3").src="assets/img/image3.jpg";
-break;
-case 4:
-document.getElementById("image4").src="assets/img/image4.jpg";
-break;
-case 5:
-document.getElementById("image5").src="assets/img/image5.jpg";
-break;
-}
+// et ici nous allons récupérer la première image
+function resetImage(img){
+  document.getElementById(img).src ='assets/images/' + img + '.jpg';
 }
